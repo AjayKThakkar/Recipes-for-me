bc526e8aaf0f16110238ef39381e2be1e79f02c9
Ajay Thakkar <40303519+AjayKThakkar@users.noreply.github.com>
2018-09-05 09:32:26 -0400

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   namespace :api do
    namespace :v1 do
      resources :ingredients, only: [:index, :show, :new, :create, :update] 
+     
   end
+    
  end
+  
 end
